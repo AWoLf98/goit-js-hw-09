4c0324fe003cadce7d2f6b5eb73a9c4ea02ab6ff
AWoLf98 <32619379+AWoLf98@users.noreply.github.com>
2024-02-11 00:21:27 +0000

    Deploying to gh-pages from @ AWoLf98/goit-js-hw-09@6546d202573cd0c913d7525d41f19fbf07ae7e48 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const r=document.querySelector("form.feedback-for");r.addEventListener("input",t=>{const o={input:"",textarea:""},e=t.currentTarget.nodeName;(e==="INPUT"||e==="TEXTAREA")&&window.localStorage.setItem("feedback-form-state",o)});
+//# sourceMappingURL=commonHelpers2.js.map
